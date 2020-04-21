6e191493decf56b5a5445e7606adc9c18b737b9c
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-04-21 22:10:20 +0530

    firebase database1

--- app/src/main/java/com/example/doctor_appointment/userlogin.java
+++ app/src/main/java/com/example/doctor_appointment/userlogin.java
@@ -6,14 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class userlogin extends AppCompatActivity {
-    public Button btnsignin;
+    public Button btnsignup,btnlogin;
+    EditText txtuname,txtpass;
+    DatabaseReference reff;
+    User user;
 
     public void init(){
 
-        btnsignin= (Button)findViewById(R.id.btn_signup);
-        btnsignin.setOnClickListener(new View.OnClickListener() {
+        btnsignup= (Button)findViewById(R.id.btn_signup);
+        btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent reg = new Intent(userlogin.this, userregister.class);
--- app/src/main/java/com/example/doctor_appointment/userregister.java
+++ app/src/main/java/com/example/doctor_appointment/userregister.java
@@ -3,12 +3,46 @@ package com.example.doctor_appointment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class userregister extends AppCompatActivity {
+    Button btnregister;
+    EditText txtfname,txtlname,txtmobile,txtemail,txtdob,txtpass;
+    DatabaseReference reff;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userregister);
+
+        txtfname=(EditText)findViewById(R.id.first_name);
+        txtlname=(EditText)findViewById(R.id.last_name);
+        txtmobile=(EditText)findViewById(R.id.mob_num);
+        txtemail=(EditText)findViewById(R.id.user_email);
+        txtdob=(EditText)findViewById(R.id.user_dob);
+        txtpass=(EditText)findViewById(R.id.user_pass);
+        btnregister=(Button)findViewById(R.id.submit_btn);
+
+        user=new User();
+        reff= FirebaseDatabase.getInstance().getReference().child("User");
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Long mob=Long.parseLong(txtmobile.getText().toString().trim());
+                user.setFirstName(txtfname.getText().toString().trim());
+                user.setLastName(txtlname.getText().toString().trim());
+                user.setMob(mob);
+                user.setEmail(txtemail.getText().toString().trim());
+                user.setDOB(txtdob.getText().toString().trim());
+                user.setPass(txtpass.getText().toString().trim());
+                reff.push().setValue(user);
+                //Toast.makeText(datainsert.this, "data inserted successfully",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
