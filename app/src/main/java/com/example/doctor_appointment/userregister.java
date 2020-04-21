ee2c0f7c7459f8ab5b6902f30bffaeff9b10d1df
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-04-21 22:16:08 +0530

    firebase database1

--- app/src/main/java/com/example/doctor_appointment/userregister.java
+++ app/src/main/java/com/example/doctor_appointment/userregister.java
@@ -3,10 +3,12 @@ package com.example.doctor_appointment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class userregister extends AppCompatActivity {
     Button btnregister;
