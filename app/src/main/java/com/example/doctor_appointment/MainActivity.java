1e791d3dfd6f25b9225c9b08ede5e7f2cd35da87
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-04-21 13:54:16 +0530

    btn1

--- app/src/main/java/com/example/doctor_appointment/MainActivity.java
+++ app/src/main/java/com/example/doctor_appointment/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.doctor_appointment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button btn1;
 
+    public void init(){
+        btn1= (Button)findViewById(R.id.buttontemp);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ask = new Intent(MainActivity.this, askdoctor.class);
+                startActivity(ask);
+
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
--- app/src/main/java/com/example/doctor_appointment/askdoctor.java
+++ app/src/main/java/com/example/doctor_appointment/askdoctor.java
@@ -2,14 +2,31 @@ package com.example.doctor_appointment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class askdoctor extends AppCompatActivity {
+    public Button btn1;
+
+    public void init(){
+        btn1= (Button)findViewById(R.id.doctor_button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent doc = new Intent(askdoctor.this, doctorlogin.class);
+                startActivity(doc);
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_askdoctor);
+        init();
     }
 }
