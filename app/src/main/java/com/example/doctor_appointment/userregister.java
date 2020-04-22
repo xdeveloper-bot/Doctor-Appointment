04086ad73d6b47a43cabb8daed63ac76998ae16c
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-04-22 14:04:18 +0530

    increament

--- app/src/main/java/com/example/doctor_appointment/userregister.java
+++ app/src/main/java/com/example/doctor_appointment/userregister.java
@@ -1,5 +1,6 @@
 package com.example.doctor_appointment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,14 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class userregister extends AppCompatActivity {
     Button btnregister;
     EditText txtfname,txtlname,txtmobile,txtemail,txtdob,txtpass;
     DatabaseReference reff;
     User user;
+    long maxid=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,18 @@ public class userregister extends AppCompatActivity {
 
         user=new User();
         reff= FirebaseDatabase.getInstance().getReference().child("User");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                    maxid=(dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
 
         btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +60,7 @@ public class userregister extends AppCompatActivity {
                 user.setEmail(txtemail.getText().toString().trim());
                 user.setDOB(txtdob.getText().toString().trim());
                 user.setPass(txtpass.getText().toString().trim());
+                reff.child(String.valueOf(maxid+1)).setValue(user);
                 reff.push().setValue(user);
                 Toast.makeText(userregister.this, "data inserted successfully",Toast.LENGTH_LONG).show();
             }
