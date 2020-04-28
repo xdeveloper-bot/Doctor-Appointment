3f1c407362738db828304dde1f73cd5a637f3a20
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-04-28 13:19:23 +0530

    color search

--- app/src/main/java/com/example/doctor_appointment/user_search.java
+++ app/src/main/java/com/example/doctor_appointment/user_search.java
@@ -1,14 +1,28 @@
 package com.example.doctor_appointment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class user_search extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search);
+
+        toolbar=findViewById(R.id.usea_toolbar);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
