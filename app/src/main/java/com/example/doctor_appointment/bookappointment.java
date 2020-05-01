66f3f2bb89dac9e569625536de2da443bbe7cce2
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-01 17:16:28 +0530

    book

--- app/src/main/java/com/example/doctor_appointment/bookappointment.java
+++ app/src/main/java/com/example/doctor_appointment/bookappointment.java
@@ -1,14 +1,28 @@
 package com.example.doctor_appointment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class bookappointment extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookappointment);
+
+        toolbar = findViewById(R.id.bapp_toolbar);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
