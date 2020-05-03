df9f3495578989064608aca8e3ca95b76aaa89a1
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-03 19:31:21 +0530

    variable no of CardView

--- app/src/main/java/com/example/doctor_appointment/doctor_list.java
+++ app/src/main/java/com/example/doctor_appointment/doctor_list.java
@@ -3,11 +3,15 @@ package com.example.doctor_appointment;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,12 +22,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class doctor_list extends AppCompatActivity {
-    Button btnBook;
-    ImageView imgProfile, imgArrow;
-    TextView txtName, txtSpecialty, txtHospital;
     EditText txtSearch;
     String valFromBookAppointment;
-    Integer intNum = 1;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
 
@@ -46,13 +46,25 @@ public class doctor_list extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()){
+                            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+                            LayoutInflater li = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                             for (QueryDocumentSnapshot document : task.getResult()){
                                 Log.d("TAG", document.getId() + " => " + document.getData());
                                 Log.d("TAG", document.getId() + " => " + document.get("name"));
                                 // Create multiple Card Layout
+                                View tempView = li.inflate(R.layout.doctorListTemplete, null);
 
+                                TextView txtName = (TextView) tempView.findViewById(R.id.dlst_name);
+                                TextView txtSpecialty = (TextView) tempView.findViewById(R.id.dlst_specialty);
+                                TextView txtHospital = (TextView) tempView.findViewById(R.id.dlst_hospital);
+                                ImageView imgProfile, imgArrow;
+                                Button btnBook;
 
-                                intNum += 1;
+                                txtName.setText(document.get("name").toString());
+                                txtSpecialty.setText(document.get("designation").toString());
+                                txtHospital.setText(document.get("hospital").toString());
+
+                                mainLayout.addView(tempView);
                             }
                         } else {
                             Log.d("TAG", "Error getting documents: ",task.getException());
