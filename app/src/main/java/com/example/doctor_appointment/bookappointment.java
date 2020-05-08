9ce05458622ec440066b85816c8dddb9f691d464
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-08 18:14:44 +0530

    1

--- app/src/main/java/com/example/doctor_appointment/bookappointment.java
+++ app/src/main/java/com/example/doctor_appointment/bookappointment.java
@@ -9,23 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class bookappointment extends AppCompatActivity {
     Toolbar toolbar;
-    TextView txtwomen, txtskin, txtgeneral, txtchild, txtdental, txteye, txtmental, txtbrain, txtheart, txtcancer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookappointment);
-
-        toolbar = findViewById(R.id.bapp_toolbar);
-        txtwomen = findViewById(R.id.bapp_women);
-        txtskin = findViewById(R.id.bapp_skin);
-        txtgeneral = findViewById(R.id.bapp_general);
-        txtchild = findViewById(R.id.bapp_child);
-        txtdental = findViewById(R.id.bapp_dental);
-        txteye = findViewById(R.id.bapp_eye);
-        txtmental = findViewById(R.id.bapp_mental);
-        txtheart = findViewById(R.id.bapp_heart);
-        txtcancer = findViewById(R.id.bapp_cancer);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,65 +61,4 @@ public class bookappointment extends AppCompatActivity {
         startActivity(i);
         overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
-
-    /*public void btnWomenClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtwomen.getText());
-        startActivity(i);
-    }
-
-    public void btnSkinClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtskin.getText());
-        startActivity(i);
-    }
-
-    public void btnGeneralClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtgeneral.getText());
-        startActivity(i);
-    }
-
-    public void btnChildClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtchild.getText());
-        startActivity(i);
-    }
-
-    public void btnDentalClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtdental.getText());
-        startActivity(i);
-    }
-
-    public void btnEyeClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txteye.getText());
-        startActivity(i);
-    }
-
-    public void btnMentalClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtmental.getText());
-        startActivity(i);
-    }
-
-    public void btnBrainClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtbrain.getText());
-        startActivity(i);
-    }
-
-    public void btnHeartClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtheart.getText());
-        startActivity(i);
-    }
-
-    public void btnCancerClickAct(View v) {
-        Intent i = new Intent(this, doctor_list.class);
-        i.putExtra("value", txtcancer.getText());
-        startActivity(i);
-    }*/
-
 }
