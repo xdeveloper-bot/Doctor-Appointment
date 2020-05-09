2646d935b97872006f3647503691537de4471b5a
Yash Gupta <62286270+Yash25gupta@users.noreply.github.com>
2020-05-09 14:24:58 +0530

    as

--- app/src/main/java/com/example/doctor_appointment/doctor_list.java
+++ app/src/main/java/com/example/doctor_appointment/doctor_list.java
@@ -2,6 +2,7 @@ package com.example.doctor_appointment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -11,10 +12,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -25,13 +28,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Calendar;
+
 public class doctor_list extends AppCompatActivity {
     EditText txtSearch;
     FirebaseAuth fAuth;
     String valFromActivity;
     FirebaseFirestore fStore;
+    ConstraintLayout dateTimeLayout;
+    TextView txtDate, txtTime;
+    Button btnSelectDate, btnSelectTime, btnSubmit;
+    DatePicker datePicker;
+    TimePicker timePicker;
     Toolbar toolbar;
-    String searchText;
+    String searchText, docID, time, date;
     Integer intNum = 0;
 
     @Override
@@ -41,6 +51,15 @@ public class doctor_list extends AppCompatActivity {
 
         toolbar = findViewById(R.id.dlst_toolbar);
         txtSearch = findViewById(R.id.dlst_searchtxt);
+        dateTimeLayout = findViewById(R.id.dlst_dateTimeLayout);
+        txtDate = findViewById(R.id.dlst_date);
+        txtTime = findViewById(R.id.dlst_time);
+        btnSelectDate = findViewById(R.id.dlst_selectdate);
+        btnSelectTime = findViewById(R.id.dlst_selecttime);
+        btnSubmit = findViewById(R.id.dlst_submitBtn);
+        datePicker = findViewById(R.id.dlst_datePicker);
+        timePicker = findViewById(R.id.dlst_timePicker);
+
         valFromActivity = getIntent().getExtras().getString("value");
         txtSearch.setText(valFromActivity);
 
@@ -116,24 +135,73 @@ public class doctor_list extends AppCompatActivity {
                 });
 
         //Next line here
+        txtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSubmit.setVisibility(View.GONE);
+                timePicker.setVisibility(View.VISIBLE);
+                btnSelectTime.setVisibility(View.VISIBLE);
+                timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                    @Override
+                    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                        time = hourOfDay + ":" + minute;
+                    }
+                });
+            }
+        });
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSubmit.setVisibility(View.GONE);
+                datePicker.setVisibility(View.VISIBLE);
+                btnSelectDate.setVisibility(View.VISIBLE);
+                Calendar calender = Calendar.getInstance();
+                datePicker.init(calender.get(Calendar.YEAR), calender.get(Calendar.MONTH),
+                        calender.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+                            @Override
+                            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                date = datePicker.getDayOfMonth() + "-" + datePicker.getMonth() + "-" + datePicker.getYear();
+                            }
+                        });
+            }
+        });
+
+        btnSelectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.setVisibility(View.GONE);
+                btnSelectTime.setVisibility(View.GONE);
+                btnSubmit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.setVisibility(View.GONE);
+                btnSelectDate.setVisibility(View.GONE);
+                btnSubmit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "DoctorID: " + docID + " Date: " +
+                        date + " Time: " + time, Toast.LENGTH_SHORT).show();
+                dateTimeLayout.setVisibility(View.GONE);
+            }
+        });
 
     }
 
     View.OnClickListener btnClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getTag().toString()){
-                case "Book 0":
-                    // action
-                    break;
-                case "Book 1":
-                    // as
-                    break;
-
-                default:
-                    break;
-            }
-            Toast.makeText(getApplicationContext(), "Button" + v.getId(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), "Button" + v.getId(), Toast.LENGTH_SHORT).show();
+            docID = v.getTag().toString();
+            dateTimeLayout.setVisibility(View.VISIBLE);
         }
     };
 
